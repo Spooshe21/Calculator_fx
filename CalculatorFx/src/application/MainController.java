c34173e93f92e40cfb1206ce7491a7aaa1420dcc
Spooshe21 <spoorthipshetty1234@gmail.com>
2024-06-16 20:49:14 +0530

    Commit

---
+++ CalculatorFx/src/application/MainController.java
@@ -0,0 +1,74 @@
+/********************************************************************************************
+ *   COPYRIGHT (C) 2022 CREVAVI TECHNOLOGIES PVT LTD
+ *   The reproduction, transmission or use of this document/file or its
+ *   contents is not permitted without written authorization.
+ *   Offenders will be liable for damages. All rights reserved.
+ *---------------------------------------------------------------------------
+ *   Purpose:  MainController class file for the Calculator application.
+ *   Project:  Calculator
+ *   Platform: Cloud Service Provider Independent
+ *   Compiler: JavaSE-11
+ *   IDE:      Eclipse IDE for Enterprise Java and Web Developers (includes Incubating components)
+ *             Version: 2021-09 (4.21.0)
+ *             Build id: 20210910-1417
+ ********************************************************************************************/
+
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+
+/**
+ * The MainController class controls the user interactions in the application.
+ */
+public class MainController {
+    
+    @FXML
+    private Label result;
+    private long number1 = 0;
+    private String operator = "";
+    private boolean start = true;
+    private Modal modal = new Modal();
+    
+    /**
+     * Processes the numbers entered by the user.
+     * 
+     * @param event The event triggered by pressing a number button.
+     */
+    @FXML
+    public void processNumbers(ActionEvent event) {
+        if (start) {
+            result.setText("");
+            start = false;
+        }
+        String value = ((Button) event.getSource()).getText();
+        result.setText(result.getText() + value);
+    }
+    
+    /**
+     * Processes the operators entered by the user.
+     * 
+     * @param event The event triggered by pressing an operator button.
+     */
+    @FXML
+    public void processOperators(ActionEvent event) {
+        String value = ((Button) event.getSource()).getText();
+        if (!value.equals("=")) {
+            if (!operator.isEmpty())
+                return;
+            operator = value;
+            number1 = Long.parseLong(result.getText());
+            result.setText("");
+        } else {
+            if (operator.isEmpty())
+                return;
+            long number2 = Long.parseLong(result.getText());
+            float output = modal.calculate(number1, number2, operator);
+            result.setText(String.valueOf(output));
+            operator = "";
+            start = true;
+        }
+    }
+}
---
+++ CalculatorFx/src/application/Modal.java
@@ -0,0 +1,47 @@
+/********************************************************************************************
+ *   COPYRIGHT (C) 2022 CREVAVI TECHNOLOGIES PVT LTD
+ *   The reproduction, transmission or use of this document/file or its
+ *   contents is not permitted without written authorization.
+ *   Offenders will be liable for damages. All rights reserved.
+ *---------------------------------------------------------------------------
+ *   Purpose:  Modal class file for the Calculator application.
+ *   Project:  Calculator
+ *   Platform: Cloud Service Provider Independent
+ *   Compiler: JavaSE-11
+ *   IDE:      Eclipse IDE for Enterprise Java and Web Developers (includes Incubating components)
+ *             Version: 2021-09 (4.21.0)
+ *             Build id: 20210910-1417
+ ********************************************************************************************/
+
+package application;
+
+/**
+ * The Modal class provides methods for performing mathematical calculations.
+ */
+public class Modal {
+    
+    /**
+     * Performs the calculation based on the provided operator and operands.
+     * 
+     * @param number1 The first operand.
+     * @param number2 The second operand.
+     * @param operator The operator to perform the calculation.
+     * @return The result of the calculation.
+     */
+    public float calculate(long number1, long number2, String operator) {
+        switch (operator) {
+            case "+":
+                return number1 + number2;
+            case "-":
+                return number1 - number2;
+            case "*":
+                return number1 * number2;
+            case "/":
+                if (number2 == 0)
+                    return 0; // Handling division by zero
+                return number1 / number2;
+            default:
+                return 0; // Invalid operator
+        }
+    }
+}
